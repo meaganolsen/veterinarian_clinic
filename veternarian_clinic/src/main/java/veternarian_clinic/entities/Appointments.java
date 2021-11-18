4943a2f74af2729ad9595242fe464a162404a8a3
Meagan Olsen <87912482+meaganolsen@users.noreply.github.com>
2021-11-18 11:56:39 -0700

    Add files via upload

---
+++ veternarian_clinic/src/main/java/veternarian_clinic/entities/Appointments.java
@@ -0,0 +1,10 @@
+package veternarian_clinic.entities;
+
+public class Appointments {
+  
+  private int clientId;
+  private int petId;
+  private String scheduledAppts;
+  private String apptType;
+
+}
