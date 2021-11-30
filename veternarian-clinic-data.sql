eea9be69fdada2170c6607c30dc5f9a680f7ee8a
Meagan Olsen <87912482+meaganolsen@users.noreply.github.com>
2021-11-30 07:24:51 -0700

    Update veternarian-clinic-data.sql

--- veternarian-clinic-data.sql
+++ veternarian-clinic-data.sql
@@ -12,11 +12,20 @@ INSERT INTO BILLING (client_id, pet_id, balance_due) VALUES (1, 1, 00.00);
 INSERT INTO BILLING (client_id, pet_id, balance_due) VALUES (2, 3, 50.57); 
 INSERT INTO BILLING (client_id, pet_id, balance_due) VALUES (3, 4, 100.17); 
 
-INSERT INTO APPOINTMENTS (pet_id, scheduled_appt,appt_type) VALUES (1, 'JANUARY 15, 2022 3:45PM','NAIL TRIM');
-INSERT INTO APPOINTMENTS (pet_id, scheduled_appt,appt_type) VALUES (2, 'JANUARY 15, 2022 4:00PM','EXAM');
-INSERT INTO APPOINTMENTS (pet_id, scheduled_appt,appt_type) VALUES (3, 'DECEMBER 11, 2021 3:00PM','VACCINES');
-INSERT INTO APPOINTMENTS (pet_id, scheduled_appt,appt_type) VALUES (4, 'NOVEMBER 20, 2021 3:45PM','EXAM');
+INSERT INTO APPOINTMENTS (pet_id, scheduled_appt) VALUES (1, '2022-01-22 03:45:00');
+INSERT INTO APPOINTMENTS (pet_id, scheduled_appt) VALUES (2, '2021-12-28 08:45:00');
+INSERT INTO APPOINTMENTS (pet_id, scheduled_appt) VALUES (3, '2022-02-22 10:30:00');
+INSERT INTO APPOINTMENTS (pet_id, scheduled_appt) VALUES (4, '2021-11-29 11:45:00');
 
+INSERT INTO SERVICES (pet_id, appt_type, description) VALUES (1, 'exam', 'well pet exam');
+INSERT INTO SERVICES (pet_id, appt_type, description) VALUES (2, 'surgery', 'knee surgery');
+INSERT INTO SERVICES (pet_id, appt_type, description) VALUES (3, 'exam', 'asthma exam');
+INSERT INTO SERVICES (pet_id, appt_type, description) VALUES (4, 'vaccines', 'yearly vaccines');
+
+INSERT INTO appointment_services (appointment_id, service_id) VALUES (1, 1);
+INSERT INTO appointment_services (appointment_id, service_id) VALUES (2, 2);
+INSERT INTO appointment_services (appointment_id, service_id) VALUES (3, 3);
+INSERT INTO appointment_services (appointment_id, service_id) VALUES (4, 4);
 
 
 
